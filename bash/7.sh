d5d40f0cd46a76045bb5abb5fde3449b8e8da9b2
Oseus <dan_shelest@mail.ru>
2025-05-28 16:29:46 +0300

    28_06

--- bash/7.sh
+++ bash/7.sh
@@ -9,9 +9,9 @@ echo "$PATH" | tr ':' '\n' | while IFS= read -r dir; do
     # Проверяем существование директории
     if [ -d "$dir" ]; then
         # Подсчитываем количество файлов (не включая директории)
-        #count=$(find "$dir" -maxdepth 1 -type f 2>/dev/null | wc -l)
+        count=$(find "$dir" -maxdepth 1 -type f 2>/dev/null | wc -l)
 	#Если нужно считать не только файлики, но и ссылки и прочие приколы
-	count=$(find "$dir" -maxdepth 1 \( -type f -o -type l \) 2>/dev/null | wc -l)
+	#count=$(find "$dir" -maxdepth 1 \( -type f -o -type l \) 2>/dev/null | wc -l)
         printf "Путь: %-40s | Файлов: %d\n" "$dir" "$count"
     else
         printf "Путь: %-40s | ОШИБКА: Директория не существует\n" "$dir"
